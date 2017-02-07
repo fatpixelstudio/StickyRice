65eca5cba2626d1a7cad09a9faeb8049854851f9
Marijn Tijhuis <post@fatpixel.nl>
2017-02-07 17:03:22 +0100

    Add default page template.
    Everything is still under construction, you know…

---
+++ page.php
@@ -0,0 +1,20 @@
+<?php get_header(); ?>
+
+	<main class="contain-padding">
+
+		<h1 class="alpha-heading">Pagina niet gevonden</h1>
+		<div class="copy">
+			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+				<h1><?php the_title(); ?></h1>
+
+				<?php the_content(); ?>
+			<?php endwhile; else: ?>
+
+				<p>Pagina niet gevonden.</p>
+
+			<?php endif; ?>
+		</div>
+
+	</main>
+
+<?php get_footer(); ?>
