1ee802e0666ecaeff45d621dd0bdcef3cb7253c2
Marijn Tijhuis <post@fatpixel.nl>
2017-02-23 10:59:59 +0100

    Update some basics.

--- page.php
+++ page.php
@@ -2,12 +2,13 @@
 
 	<main class="contain-padding">
 
-		<h1 class="alpha-heading">Pagina niet gevonden</h1>
 		<div class="copy">
 			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
 				<h1><?php the_title(); ?></h1>
 
 				<?php the_content(); ?>
+
 			<?php endwhile; else: ?>
 
 				<p>Pagina niet gevonden.</p>
