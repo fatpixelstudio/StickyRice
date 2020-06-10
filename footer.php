13333e2ddbc4d8cf68c1da33cc147c4db2aa1997
Marijn Tijhuis <post@fatpixel.nl>
2020-06-10 12:05:21 +0200

    Change Analytics to gtag, and add anonymize ip as default.

--- footer.php
+++ footer.php
@@ -15,14 +15,13 @@
 
 	<?php wp_footer(); ?>
 
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-141682345-9"></script>
 	<script>
-		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-		})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-		ga('create', 'UA-XXXXXXXX-XX', 'auto');
-		ga('send', 'pageview');
+		window.dataLayer = window.dataLayer || [];
+		function gtag(){dataLayer.push(arguments);}
+		gtag('js', new Date());
+		gtag('config', 'UA-XXXXXXXXX-X', { 'anonymize_ip': true });
 	</script>
 </body>
 </html>
